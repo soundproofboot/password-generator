32db169fa82ec98e4746dadf7d13de5f7fac1f69
C Bares <colinedwinbares@gmail.com>
2021-12-05 16:47:20 -0600

    worked alerts into generatePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,45 @@
 // Assignment code here
+var length;
+var lowercase;
+var uppercase;
+var numbers;
+var specialCharacters;
+
+var generatePassword = function() {
+  length = getLength();
+  lowercase = window.confirm('Press okay to include lowercase letters.');
+  uppercase = window.confirm('Press okay to include uppercase letters.');
+  numbers = window.confirm('Press okay to include numbers.');
+  specialCharacters = window.confirm('Press okay to include special characters');
+}
+
+var getLength = function() {
+  var lengthInput = parseInt(window.prompt('How many characters in your password? (Must be a number between 8 and 128)'));
+  if (lengthInput > 7 && lengthInput < 129) {
+    return lengthInput;
+  }
+  else {
+    return getLength();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Get references to the #generate element
