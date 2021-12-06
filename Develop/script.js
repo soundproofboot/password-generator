86e18cf60f00922efe8907085e7fee82ecdd9d42
C Bares <colinedwinbares@gmail.com>
2021-12-05 18:44:12 -0600

    all prompts set up, validate length, store which characters are allowed, randomly access

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,15 @@
 // Assignment code here
-var length;
-var lowercase;
-var uppercase;
-var numbers;
-var specialCharacters;
-
 var generatePassword = function() {
-  length = getLength();
-  lowercase = window.confirm('Press okay to include lowercase letters.');
-  uppercase = window.confirm('Press okay to include uppercase letters.');
-  numbers = window.confirm('Press okay to include numbers.');
-  specialCharacters = window.confirm('Press okay to include special characters');
+  let length = getLength();
+  let charSet = confirmSets();
+  let generated = ''
+  for(let i = 0; i < length; i++) {
+    randomIndex = Math.floor(Math.random() * charSet.length);
+    generated += charSet[randomIndex];
+  }
+  return generated;
 }
+
 
 var getLength = function() {
   var lengthInput = parseInt(window.prompt('How many characters in your password? (Must be a number between 8 and 128)'));
@@ -23,7 +21,35 @@ var getLength = function() {
   }
 }
 
+var confirmSets = function() {
+  let allowedCharacters = ''
+  let lowercase = window.confirm('Press okay to include lowercase letters.');
+  if (lowercase) {
+    allowedCharacters += 'abcdefghijklmnopqrstuvwxyz';
+  }
 
+  let uppercase = window.confirm('Press okay to include uppercase letters.');
+  if (uppercase) {
+    allowedCharacters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  }
+
+  let numbers = window.confirm('Press okay to include numbers.');
+  if (numbers) {
+    allowedCharacters += '0123456789';
+  }
+
+  let specialCharacters = window.confirm('Press okay to include special characters');
+  if (specialCharacters) {
+    allowedCharacters += ` !"#$%&'()*+,-./:;<=>?@[\]^_{|}~` + '`';
+  }
+  if (!allowedCharacters) {
+    window.alert('You must select at least one character set to include in your password.')
+    return confirmSets();
+  }
+  else {
+    return allowedCharacters;
+  }
+}
 
 
 
